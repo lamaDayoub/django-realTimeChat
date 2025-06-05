9792c01a21ddf0ec11f9e812367aaf89353f76bf
lamaDayoub <lama1e2dayoub@gmail.com>
2025-06-06 01:21:04 +0300

    adding private chats but without dropdown to lest them all

--- a_core/settings.py
+++ a_core/settings.py
@@ -101,6 +101,15 @@ CHANNEL_LAYERS = {
         "BACKEND" :"channels.layers.InMemoryChannelLayer", 
     }
 }
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [('redis://default:XxkKfRhdTxqLXAKGjQKchUtIBLOkNybL@redis-uimp.railway.internal:6379')],
+           
+#         },
+#     },
+# }
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
